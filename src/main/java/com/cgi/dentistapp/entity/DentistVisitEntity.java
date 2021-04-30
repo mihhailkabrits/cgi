ffa11009f391d1963fde5b433dffb56d4d23d715
mihha <mihhail.kabrits@ttu.ee>
2021-04-30 14:16:12 +0300

    2nd

---
+++ src/main/java/com/cgi/dentistapp/entity/DentistVisitEntity.java
@@ -0,0 +1,16 @@
+package com.cgi.dentistapp.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "dentist_visit")
+public class DentistVisitEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    //TODO im
+    // plementation
+
+}
